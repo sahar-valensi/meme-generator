84ef8815b281887d9bc95000b76b164b38fc2a0f
Sahar Valensi <saharva3@gmail.com>
2025-08-11 15:40:08 +0300

    add canvas to editor

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ var gImgs = [
   { id: 3, url: "../assets/meme-img/3.jpg", title: "Meme 3" },
 ];
 var gCurrImgUrl = "";
+var gElCanvas;
+var gCtx;
 
 const modal = document.getElementById("img-modal");
 const modalImg = document.getElementById("modal-image");
@@ -57,11 +59,13 @@ function onClosePreview() {
 function onOpenEditor() {
   onClosePreview();
 
-  var elHome   = document.getElementById('home');
-  var elEditor = document.getElementById('editor');
+  var elHome = document.getElementById("home");
+  var elEditor = document.getElementById("editor");
 
-  if (elHome)   elHome.hidden = true;
+  if (elHome) elHome.hidden = true;
   if (elEditor) elEditor.hidden = false;
+
+  initEditor();
 }
 
 function onModalClick(event) {
@@ -79,6 +83,41 @@ function initModal() {
   document.addEventListener("keydown", onKeyDown);
 }
 
+function initEditor() {
+  gElCanvas = document.getElementById("meme-canvas");
+  if (!gElCanvas) return;
+  gCtx = gElCanvas.getContext("2d");
+
+  gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height);
+
+  drawBaseImage();
+}
+
+function drawBaseImage() {
+  var src = gCurrImgUrl || (gImgs[0] && gImgs[0].url);
+  if (!src) return;
+
+  var img = new Image();
+  img.onload = function () {
+    var cw = gElCanvas.width;
+    var ch = gElCanvas.height;
+
+    gCtx.clearRect(0, 0, cw, ch);
+
+    var rect = getContainRect(img.width, img.height, cw, ch);
+    gCtx.drawImage(img, rect.x, rect.y, rect.w, rect.h);
+  };
+  img.src = src;
+}
+
+function getContainRect(iw, ih, cw, ch) {
+  var ratio = Math.min(cw / iw, ch / ih);
+  var w = Math.round(iw * ratio);
+  var h = Math.round(ih * ratio);
+  var x = Math.round((cw - w) / 2);
+  var y = Math.round((ch - h) / 2);
+  return { x: x, y: y, w: w, h: h };
+}
 // initModal();
 
 // window.onOpenPreview  = onOpenPreview;
