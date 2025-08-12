7b9464ae701770153641f6dc2d9ab7b58d3d4076
Sahar Valensi <saharva3@gmail.com>
2025-08-12 11:27:12 +0300

    fix imgs path

--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,8 +1,8 @@
 "use strict";
 var gImgs = [
-  { id: 1, url: "../assets/meme-imgs/1.jpg", title: "Meme 1" },
-  { id: 2, url: "../assets/meme-imgs/2.jpg", title: "Meme 2" },
-  { id: 3, url: "../assets/meme-imgs/3.jpg", title: "Meme 3" },
+  { id: 1, url: "assets/meme-imgs/1.jpg", title: "Meme 1" },
+  { id: 2, url: "assets/meme-imgs/2.jpg", title: "Meme 2" },
+  { id: 3, url: "assets/meme-imgs/3.jpg", title: "Meme 3" },
 ];
 
 var gMeme = {
